bf693739c0d600c2ad464910a80ae8e7099868bb
sergey2010 <ksd921020@yandex.ru>
2017-03-16 15:33:04 +0500

    1 commit developer2

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -4,5 +4,7 @@
 public class Team {
     public static void main(String[] args) {
         System.out.println("Hello Team");
+        System.out.println("Job developer 2");
+        System.out.println("Job developer 2");
     }
 }
