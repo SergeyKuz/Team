bb5608d87cea6af41171cf7b182c6b4e25b2446b
sergey2010 <ksd921020@yandex.ru>
2017-03-16 17:16:29 +0500

    2

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -10,5 +10,6 @@ public class Team {
         System.out.println("Job developer 2");
         System.out.println("Hello from branch developer1");
         System.out.println("This is local branch");
+        System.out.println("localb => remoteb");
     }
 }
