6b09f70e8c919b84c9b771c9d8109a018021e47c
sergey2010 <ksd921020@yandex.ru>
2017-03-16 17:31:19 +0500

    jbbbjkbhiijkljkljkj

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -5,13 +5,7 @@ public class Team {
     public static void main(String[] args) {
         System.out.println("Hello Team");
         System.out.println("Job developer 1");
-        System.out.println("Job developer 1");
-        System.out.println("Job developer 2");
-        System.out.println("Job developer 2");
-        System.out.println("Hello from branch developer1");
-        System.out.println("This is local branch");
-        System.out.println("localb => remoteb");
-        System.out.println("localb => remoteb2");
+
         
 
     }
