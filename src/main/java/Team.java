e5c520214e251cfbf957e83fca730dee53a52318
sergey2010 <ksd921020@yandex.ru>
2017-03-16 16:24:24 +0500

    2 commit developer8r

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -9,5 +9,6 @@ public class Team {
         System.out.println("Job developer 2");
         System.out.println("Job developer 2");
         System.out.println("Hello from branch developer1");
+        System.out.println("This is local branch");
     }
 }
