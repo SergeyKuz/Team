6144f4e97c634958adb0ca22bc14090afdada718
SergeyKuz <git.kuznetsov@yandex.ru>
2017-03-16 17:32:37 +0500

    Update Team.java

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -3,16 +3,6 @@
  */
 public class Team {
     public static void main(String[] args) {
-        System.out.println("Hello Team");
-        System.out.println("Job developer 1");
-        System.out.println("Job developer 1");
-        System.out.println("Job developer 2");
-        System.out.println("Job developer 2");
-        System.out.println("Hello from branch developer1");
-        System.out.println("This is master branch");
-        System.out.println("This is master branch Idea");
-        System.out.println("Hello Team2");
-        
-        System.out.println("Hello Team2");
+
     }
 }
